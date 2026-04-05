6f3639b0b59b6c03b1fc73a14ffec5ec37174f57
dh1180 <mjdh1936@gmail.com>
2026-04-05 13:56:15 +0900

    init: 프로젝트 초기 세팅
    - Spring Boot (Java 21, Gradle) 기반 프로젝트 생성
    - 주요 의존성 추가
      - Spring WebMVC, Data JPA, Security (OAuth2)
      - MySQL, AWS S3 SDK, Dotenv, Swagger(Springdoc), Lombok
    - 글로벌 설정 및 공통 모듈 구성
      - SecurityConfig, SwaggerConfig, AmazonConfig, DotEnvConfig 세팅
      - 공통 API 응답 템플릿(ApiResponse) 작성
      - 전역 예외 처리(global/exception) 기본 구조 생성

---
+++ src/main/java/com/Jjambbong/PayLens/PayLensApplication.java
@@ -0,0 +1,13 @@
+package com.Jjambbong.PayLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jjambbong/PayLens/PayLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jjambbong.PayLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
