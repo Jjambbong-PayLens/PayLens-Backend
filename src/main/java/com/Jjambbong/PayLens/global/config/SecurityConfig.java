288bc12424fe4cca7af4ae14e6f0b0ae811ad830
Ophelia0419 <wnalstj016@naver.com>
2026-06-17 23:33:35 +0900

    feat: 등업 대기 노무사 목록 조회 api 구현

--- src/main/java/com/Jjambbong/PayLens/global/config/SecurityConfig.java
+++ src/main/java/com/Jjambbong/PayLens/global/config/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig {
 
                                 "/health",
                                 "/api/consultation/**",
-                                "/api/consultants/**"
+                                "/api/consultants/**",
+                                "/api/test/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
